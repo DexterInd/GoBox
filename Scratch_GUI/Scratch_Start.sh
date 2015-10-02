c4afbec746c674a5def61e2ab3afbc5ec54fa9e3
Dexter Industries <dxtrindustries@gmail.com>
2015-10-02 16:27:46 +0400

    Add Scratch.  Change links to GoBox

---
+++ Scratch_GUI/Scratch_Start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+screen -d -r local
+sudo python /home/pi/Desktop/GoBox/Scratch_GUI/Scratch_Start.py
+
+##
+# Below is the graveyard of calls unsuccessfully tried to make this word.  
+# sudo lxterminal --command "sudo xhost +"
+#sudo lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
+#lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
