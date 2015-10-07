2410e414ef15a43a53aad033d0ac11b6d9bef255
Dexter Industries <dxtrindustries@gmail.com>
2015-10-07 15:09:04 +0400

    Update the install script

--- Scratch_GUI/install_scratch_start.sh
+++ Scratch_GUI/install_scratch_start.sh
@@ -4,10 +4,6 @@
 # Dev Notes:
 # Helpful Link on Bin Paths:  http://www.cyberciti.biz/faq/how-do-i-find-the-path-to-a-command-file/
 
-sudo apt-get --purge remove python-wxgtk2.8 python-wxtools wx2.8-i18n
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
-sudo apt-get install python-psutil --force-yes
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
 
 # Copy shortcut to desktop.
 cp /home/pi/Desktop/GoBox/Scratch_GUI/Scratch_Start.desktop /home/pi/Desktop
