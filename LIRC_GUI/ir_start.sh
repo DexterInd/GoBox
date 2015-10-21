2dc9c036e73e05a3efff61b91a78ed34bebf3fd3
Karan Nayan <karan25991@gmail.com>
2015-10-21 12:30:11 +0530

    IR receiver GUI and python library added

---
+++ LIRC_GUI/ir_start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+screen -d -r local
+sudo python /home/pi/Desktop/GoBox/LIRC_GUI/ir_gui.py
+
+##
+# Below is the graveyard of calls unsuccessfully tried to make this word.  
+# sudo lxterminal --command "sudo xhost +"
+#sudo lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
+#lxterminal --command "sudo python /home/pi/Desktop/DexterEd/Scratch_GUI/Scratch_Start.py"
