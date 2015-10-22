4f825483baa95caa58df28e539f7bbd7c3dcb27f
Dexter Industries <dxtrindustries@gmail.com>
2015-10-22 10:00:43 +0400

    Cleanup LIRC Install

--- LIRC_GUI/install_ir_start.sh
+++ LIRC_GUI/install_ir_start.sh
@@ -4,13 +4,18 @@
 # Dev Notes:
 # Helpful Link on Bin Paths:  http://www.cyberciti.biz/faq/how-do-i-find-the-path-to-a-command-file/
 
-sudo apt-get --purge remove python-wxgtk2.8 python-wxtools wx2.8-i18n -y
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
-sudo apt-get install python-psutil --force-yes
-sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
+# Removing the following because it's installed in update_all.sh.  If you're running a custom install
+# you might need to add these back in.  
+#
+# sudo apt-get --purge remove python-wxgtk2.8 python-wxtools wx2.8-i18n -y
+# sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
+# sudo apt-get install python-psutil --force-yes
+# sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-i18n --force-yes
 
+# Remove any old shortcut from the desktop.
+sudo rm /home/pi/Desktop/GoBox/LIRC_GUI/ir_start.desktop
 # Copy shortcut to desktop.
-cp /home/pi/Desktop/GoBox/LIRC_GUI/ir_start.desktop /home/pi/Desktop
+sudo cp /home/pi/Desktop/GoBox/LIRC_GUI/ir_start.desktop /home/pi/Desktop
 # Make shortcut executable
 sudo chmod +x /home/pi/Desktop/ir_start.desktop
 # Make ir_start.sh executable.
