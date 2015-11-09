5aa5a39388cf8e9f25fdb9e3fe2283ed5effb221
Dexter Industries <dxtrindustries@gmail.com>
2015-11-09 15:15:25 +0400

    Install now adds links to Example Programs

--- Scratch_GUI/install_scratch_start.sh
+++ Scratch_GUI/install_scratch_start.sh
@@ -18,6 +18,12 @@ sudo chmod 777 /home/pi/Desktop/GoBox/Scratch_GUI/selected_state
 sudo chmod 777 /home/pi/Desktop/GoBox/Scratch_GUI/error_log
 sudo chmod 777 /home/pi/nohup.out
 
+# Install Scratch Example Shortcuts for the Products
+# This will creat symbolic links to the various example scripts.  https://blog.bartbania.com/raspberry_pi/create-symbolic-links-in-linux/
+ln -s /home/pi/Desktop/GrovePi/Software/Scratch/Grove_Examples/ GrovePi
+ln -s /home/pi/Desktop/GoPiGo/Software/Scratch/Examples/ GoPiGo
+ln -s /home/pi/Desktop/BrickPi_Scratch/Examples/ BrickPi
+
 # Remove Scratch Shortcuts if they're there.
 sudo rm /home/pi/Desktop/BrickPi_Scratch_Start.desktop
 sudo rm /home/pi/Desktop/GoPiGo_Scratch_Start.desktop
